53d6f14846d77b91b2b8e8dd265d323f65c92548
DESKTOP-T152T5M\pc <0gabrielvolponi0@gmail.com>
2024-09-04 00:56:11 -0300

    pt1 pre-requisitos form

--- cypress/e2e/formCalculoFrete.cy.js
+++ cypress/e2e/formCalculoFrete.cy.js
@@ -1,19 +1,64 @@
 describe('template spec', () => {
+  const cepValidos = [
+    '08090284',
+    '05407002'
+  ]
 
-  beforeEach(() => {
-      cy.visit(Cypress.env('url')).then(()=>{
-        cy.wait(10000)
+  const cepValidoRandom = cepValidos[Math.floor(Math.random()*cepValidos.length)]
+
+
+  beforeAll(() => {
+
+    cy.visit(Cypress.env('url')).then(() => {
+      cy.wait(10000)
+    })
+
+
+  });
+
+  describe('Pré-requisitos', () => {
+
+    beforeEach(() => {
+      //tomando locators
+      cy.get('[alt="banner"]').as('bannerPromotion')
+      cy.get('[id="originPostcode"]').as('inputCepOrigem')
+      cy.get('[id="object_format"]').as('selectFormato')
+      cy.get('[id="weight"]').as('selectPeso')
+
+    });
+
+    it('Validar Url', () => {
+      cy.url().should('contains', Cypress.env('url'))
+    });
+
+    it('Validar Title', () => {
+      cy.title().should('contains', 'SuperFrete')
+    });
+
+    it('Validar Banner ', () => {
+      cy.get('@bannerPromotion').should('be.visible')
+    });
+
+    it('Validar input Cep Origem -  preenchimento ', () => {
+      cy.get('@inputCepOrigem').should('be.visible').type(cepValidoRandom, { delay: 0 }).then(()=>{
+        cy.get('@inputCepOrigem').then($inputCepOrigemVal => {
+          const valorInputcepOrigem = $inputCepOrigemVal.val().replace(/[^0-9]/g, '')
+          expect(valorInputcepOrigem).eq(cepValidoRandom)
+        })
       })
-  });
+    });
 
+    it('Validar select peso - option', () => {
+      // const = [
+
+      // ]
+      cy.get('@selectFormato').click().select('[data-value="2"]')
+    });
+
+
+  });
   //fluxos pré-requisitos
-  it('Validar Url', () => {
-    cy.url().should('contains', Cypress.env('url'))
-  });
 
-  it('Validar Title', () => {
-    cy.title().should('contains', 'SuperFrete')
-  });
 
   //fluxos principais
 
